8e59e246829350ca744024412500add872f7cc18
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-11-22 15:05:43 +0700

    Add mysql config

---
+++ configs/db_config.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
+
+@dataclass
+class MySQLConfig:
+    host: str
+    user: str
+    password: str
+    database: str
+    port: int
+
+
+def get_database_config():
+    load_dotenv()
+
+    config = {
+        "mysql": MySQLConfig(
+            host=os.getenv("MYSQL_HOST"),
+            port=os.getenv("MYSQL_PORT"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE"),
+        ),
+    }
+
+    return config
+
+config = get_database_config()
+print(config)
